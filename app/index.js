8d38418eb2bee7d5ff25ad399ff64ceeeb27912a
Samuel Faber <samuel.faber@live.fr>
2023-06-10 20:47:21 +0200

    Intégration des différents éléments dans le 'index.js'

--- app/index.js
+++ app/index.js
@@ -1,31 +1,48 @@
-import clock from "clock";
 import * as document from "document";
-import { preferences } from "user-settings";
+import * as simpleClock from "./simple/clock";
+import * as simpleHRM from "./simple/hrm";
+import * as simpleActivity from "./simple/activity";
 
-function zeroPad(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
+// Element de la ClockFace
+let txtHour = document.getElementById("txtHour");
+let txtMin = document.getElementById("txtMin");
+let txtDate = document.getElementById("txtDate");
+let txtHRM = document.getElementById("txtHRM");
+let iconHRM = document.getElementById("iconHRM");
+let imgHRM = iconHRM.getElementById("icon");
+let statsCycle = document.getElementById("stats-cycle");
+let statsCycleItems = statsCycle.getElementsByClassName("cycle-item");
+
+/* --------- CLOCK ---------- */
+function clockCallback(data) {
+  txtHour.text = data.hour;
+  txtMin.text = data.min;
+  txtDate.text = data.date;
 }
+simpleClock.initialize("minutes", "shortDate", clockCallback);
 
-// Update the clock every minute
-clock.granularity = "minutes";
-
-// Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
-
-// Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
+/* -------- HRM ------------- */
+function hrmCallback(data) {
+  txtHRM.text = `${data.bpm}`;
+  if (data.zone === "out-of-range") {
+    imgHRM.href = "images/heart_open.png";
   } else {
-    // 24h format
-    hours = zeroPad(hours);
+    imgHRM.href = "images/heart_solid.png";
   }
-  let mins = zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  if (data.bpm !== "--") {
+    iconHRM.animate("highlight");
+  }
 }
+simpleHRM.initialize(hrmCallback);
+
+/* ------- ACTIVITY --------- */
+function activityCallback(data) {
+  statsCycleItems.forEach((item, index) => {
+    let img = item.firstChild;
+    let txt = img.nextSibling;
+    txt.text = data[Object.keys(data)[index]].pretty;
+    // Reposition the activity icon to the left of the variable length text
+    img.x = txt.getBBox().x - txt.parent.getBBox().x - img.width - 7;
+  });
+}
+simpleActivity.initialize("seconds", activityCallback);
